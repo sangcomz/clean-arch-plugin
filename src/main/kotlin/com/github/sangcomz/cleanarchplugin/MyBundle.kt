de1b719b71afb9b38899c16b28a2ef1192af482d
GitHub Action <action@github.com>
2025-06-26 04:03:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/sangcomz/cleanarchplugin/MyBundle.kt
+++ src/main/kotlin/com/github/sangcomz/cleanarchplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sangcomz.cleanarchplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sangcomz/cleanarchplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/sangcomz/cleanarchplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.sangcomz.cleanarchplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.sangcomz.cleanarchplugin.MyBundle
+import com.github.sangcomz.cleanarchplugin.services.MyProjectService
 import javax.swing.JButton
 
 
